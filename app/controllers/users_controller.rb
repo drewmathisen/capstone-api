11d8c7e6f8ec91485677d4942548519c6efa7845
d.mathisen <drew.a.mathisen@gmail.com>
2021-11-28 15:15:05 -0700

    created user index action and hardcoded user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def index
+    u = User.all
+    render json: u
+  end
+
+  def create
+    u = User.new(
+      username: "harrow",
+      email: "harrow@ninth.com",
+      password: "password"
+    )
+    u.save
+    render json: u
+  end
+
 end
